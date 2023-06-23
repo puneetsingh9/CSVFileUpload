8a33c9979e836df139c11cee06dd96c4906d9260
Tyrone <tyrone.wallace@compasspointfederal.com>
2023-06-23 12:36:41 +0000

    Update UI and pom.xml

--- spring-boot-import-csv-file-app-master/src/main/resources/ddl/Phase2.sql
+++ spring-boot-import-csv-file-app-master/src/main/resources/ddl/Phase2.sql
@@ -14,7 +14,7 @@ SET search_path TO pg_catalog,public,nibrs;
 CREATE SEQUENCE nibrs.upload_request_info_pk_seq 
 	INCREMENT BY 1
 	MINVALUE 0
-	MAXVALUE 9999999999999999999999999999
+	MAXVALUE 9223372036854775807
 	START WITH 1
 	CACHE 1
 	NO CYCLE
@@ -26,7 +26,7 @@ CREATE SEQUENCE nibrs.upload_request_info_pk_seq
 CREATE SEQUENCE nibrs.visual_request_pk_seq
 	INCREMENT BY 1
 	MINVALUE 0
-	MAXVALUE 9999999999999999999999999999
+	MAXVALUE 9223372036854775807
 	START WITH 1
 	CACHE 1
 	NO CYCLE
@@ -39,7 +39,7 @@ CREATE SEQUENCE nibrs.visual_request_pk_seq
 CREATE SEQUENCE nibrs.submission_status_pk_seq
 	INCREMENT BY 1
 	MINVALUE 0
-	MAXVALUE 9999999999999999999999999999
+	MAXVALUE 9223372036854775807
 	START WITH 1
 	CACHE 1
 	NO CYCLE
@@ -57,16 +57,13 @@ CREATE TABLE nibrs.UploadRequestInfo(
 	SubmissionStatus bigint,
 	FileAudit bigint,
     timestamp timestamp default current_timestamp
-
-	CONSTRAINT pk_upload_request_info_id PRIMARY KEY (RequestID),
-
 );
 -- ddl-end --
 
 -- object: nibrs.VisualRequest | type: TABLE --
 -- DROP TABLE nibrs.VisualRequest;
 CREATE TABLE nibrs.VisualRequest(
-	ID bigint NOT NULL DEFAULT nextval('nibrs.visual_request_pk_seq;'::regclass),
+	ID bigint NOT NULL DEFAULT nextval('nibrs.visual_request_pk_seq'::regclass),
 	Vr_Description text,
     timestamp timestamp default current_timestamp
 
@@ -77,14 +74,58 @@ CREATE TABLE nibrs.VisualRequest(
 -- object: nibrs.SubmissionStatus | type: TABLE --
 -- DROP TABLE nibrs.SubmissionStatus;
 CREATE TABLE nibrs.SubmissionStatus(
-	ID bigint NOT NULL DEFAULT nextval('nibrs.submission_status_pk_seq;'::regclass),
+	ID bigint NOT NULL DEFAULT nextval('nibrs.submission_status_pk_seq'::regclass),
 	Stat_Description text,
     timestamp timestamp default current_timestamp
 
 );
 
-
--- ddl-end --
-
-
-
+CREATE TABLE bjs_data (
+	    id int NOT NULL AUTO_INCREMENT,
+	  incident_id VARCHAR(255),
+	  data_year VARCHAR(255),
+	  offense_id VARCHAR(255),
+	  victim_id VARCHAR(255),
+	  offender_id VARCHAR(255),
+	  incident_date VARCHAR(255),
+	  incident_hour VARCHAR(255),
+	  ori VARCHAR(255),
+	  agency VARCHAR(255),
+	  county VARCHAR(255),
+	  state VARCHAR(255),
+	  agency_type VARCHAR(255),
+	  offense VARCHAR(255),
+	  offense_category VARCHAR(255),
+	  cleared_except VARCHAR(255),
+	  victim_sex VARCHAR(255),
+	  victim_age VARCHAR(255),
+	  victim_race VARCHAR(255),
+	  victim_ethnicity VARCHAR(255),
+	  victim_type VARCHAR(255),
+	  offender_sex VARCHAR(255),
+	  offender_age VARCHAR(255),
+	  offender_race VARCHAR(255),
+	  offender_ethnicity VARCHAR(255),
+	  relationship_status VARCHAR(255),
+	  clearance_status VARCHAR(255),
+	  is_retail_commercial_location VARCHAR(255),
+	  crime_type VARCHAR(255),
+	  inc_date VARCHAR(255),
+	  month_year_date VARCHAR(255),
+	  hour VARCHAR(255),
+	  hour_group VARCHAR(255),
+	  latitude VARCHAR(255),
+	  longitude VARCHAR(255),
+	  Beat VARCHAR(255),
+	  month_date VARCHAR(255),
+	  week_year_date VARCHAR(255),
+	  day_of_week VARCHAR(255),
+	  week_day VARCHAR(255),
+	  quarter VARCHAR(255),
+	  Location_Type VARCHAR(255),
+	  Method_of_Entry VARCHAR(255),
+	  Forced_NonForced VARCHAR(255),
+	  Point_of_Entry VARCHAR(255),
+PRIMARY KEY (id)
+	  
+	);
